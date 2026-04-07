63b96cb55e4717afea8f2991861ae4671e9b0c65
SatriaPK <putrasatriakurniawan@gmail.com>
2026-04-07 23:28:32 +0700

    Added RestAPI to GET mahasiswa

--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,6 @@ use App\Http\Controllers\Api\MahasiswaController;
 
 Route::get('/mahasiswa',        [MahasiswaController::class, 'index']);
 Route::post('/mahasiswa',       [MahasiswaController::class, 'store']);
+Route::get('/mahasiswa/{id}',   [MahasiswaController::class, 'show']);
 Route::put('/mahasiswa/{id}',   [MahasiswaController::class, 'update']);
 Route::delete('/mahasiswa/{id}',[MahasiswaController::class, 'destroy']);
